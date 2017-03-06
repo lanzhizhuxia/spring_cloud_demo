27b06615e3a13839b7dd04179bf3d0bc8fb99bf9
lanzhizhuxia <lanzhizhuxia@gmail.com>
2017-03-06 09:25:42 +0800

    创建子文件夹

---
+++ 00_redis/src/main/java/cloud/simple/hello/Init.java
@@ -0,0 +1,16 @@
+package cloud.simple.hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(value = 1)
+public class Init implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作<<<<<<<<<<<<<");
+  }
+
+}
