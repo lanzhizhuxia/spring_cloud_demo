43f7e974c22c75c463d99548c895a678df7e5a01
lanzhizhuxia <lanzhizhuxia@gmail.com>
2017-03-07 13:30:45 +0800

    01分布式配置

--- 00_redis/src/main/java/cloud/xq/util/RedisUtil.java
+++ 00_redis/src/main/java/cloud/xq/util/RedisUtil.java
@@ -6,13 +6,13 @@ import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisUtil {
   // Redis服务器IP
-  private static String ADDR = "192.168.0.137";
+  private static String ADDR = "10.66.1.41";
 
   // Redis的端口号
   private static int PORT = 6379;
 
   // 访问密码
-  private static String AUTH = "admin";
+  private static String AUTH = "";
 
   // 可用连接实例的最大数目，默认值为8；
   // 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
