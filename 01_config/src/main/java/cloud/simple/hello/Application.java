efd3588e5183b1e08a2ef373344ecc7714f45b8e
lanzhizhuxia <lanzhizhuxia@gmail.com>
2017-03-08 10:32:57 +0800

    更名为01_config_server

--- 01_config/src/main/java/cloud/simple/hello/Application.java
+++ 01_config/src/main/java/cloud/simple/hello/Application.java
@@ -1,13 +1,18 @@
 package cloud.simple.hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		//new SpringApplicationBuilder(Application.class).web(true).run(args);
+		SpringApplication.run(Application.class, args);
 	}
 }
