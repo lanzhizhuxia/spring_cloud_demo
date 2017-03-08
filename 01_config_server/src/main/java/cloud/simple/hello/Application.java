6863d7a1496e0339a33ce6498b8f570a2ab1f085
lanzhizhuxia <lanzhizhuxia@gmail.com>
2017-03-08 16:19:45 +0800

    配置文件测试

--- 01_config_server/src/main/java/cloud/simple/hello/Application.java
+++ 01_config_server/src/main/java/cloud/simple/hello/Application.java
@@ -1,18 +1,23 @@
 package cloud.simple.hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+// @Configuration
+// @EnableAutoConfiguration
+// @EnableDiscoveryClient 用来将config-server注册到上面配置的服务注册中心上去
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		//new SpringApplicationBuilder(Application.class).web(true).run(args);
-		SpringApplication.run(Application.class, args);
-	}
+
+  // public static void main(String[] args) {
+  // SpringApplication.run(Application.class, args);
+  // }
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(Application.class).web(true).run(args);
+  }
 }
